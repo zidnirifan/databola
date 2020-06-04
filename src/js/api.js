899e85ea9670ea6135da3ab2352483e01128a470
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-06-04 09:20:58 +0700

    some changes

--- src/js/api.js
+++ src/js/api.js
@@ -1,28 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => standings());
-document
-  .querySelector(".standings")
-  .addEventListener("click", () => standings());
-document.querySelector(".match").addEventListener("click", () => match());
+const page = window.location.hash.substr(1);
 
-// Standings
-
-const standings = () => {
-  fetch("https://api.football-data.org/v2/competitions/2001/standings", {
-    headers: {
-      "X-Auth-Token": "dc6ecbe5da084040b9bd5d42e6eb0a42",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      const standingsItem = response.standings[0].table;
-      let card = "";
-      standingsItem.forEach(function (item) {
-        card += resultsStandings(item);
-      });
-      const standingsList = document.querySelector(".standings-list");
-      standingsList.innerHTML = card;
-    });
-};
+if (page === "") {
+  document.addEventListener("DOMContentLoaded", () => standings());
+} else if (page === "standings") {
+  document.addEventListener("DOMContentLoaded", () => standings());
+} else if (page === "match") {
+  document.addEventListener("DOMContentLoaded", () => match());
+}
+document.querySelectorAll(".standings").forEach((elm) => {
+  elm.addEventListener("click", () => standings());
+});
+document.querySelectorAll(".match").forEach((elm) => {
+  elm.addEventListener("click", () => match());
+});
 
 const resultsStandings = (results) => {
   return `<tr class="standing-item">
@@ -38,27 +28,6 @@ const resultsStandings = (results) => {
       <td>${results.lost}</td>
       <td>${results.points}</td>
       </tr>`;
-};
-
-// Match
-
-const match = () => {
-  fetch("https://api.football-data.org/v2/competitions/2021/matches", {
-    headers: {
-      "X-Auth-Token": "dc6ecbe5da084040b9bd5d42e6eb0a42",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      const standingsItem = response.matches;
-      let card = "";
-      standingsItem.forEach(function (item) {
-        card += resultsMatch(item);
-      });
-      const standingsList = document.querySelector(".match-list");
-      standingsList.innerHTML = card;
-      console.log(match);
-    });
 };
 
 const resultsMatch = (results) => {
@@ -79,3 +48,48 @@ const resultsMatch = (results) => {
     </div>
   </div>`;
 };
+
+const requestPage = (parentSelector, selector, urlKey, results) => {
+  document.querySelector("#body-content").innerHTML = parentSelector;
+
+  fetch(`https://api.football-data.org/v2/competitions/2021/${urlKey}`, {
+    headers: {
+      "X-Auth-Token": "dc6ecbe5da084040b9bd5d42e6eb0a42",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      let items = response;
+      if (urlKey === "matches") {
+        items = response.matches;
+      } else if (urlKey === "standings") {
+        items = response.standings[0].table;
+      }
+      let card = "";
+      items.forEach(function (e) {
+        card += results(e);
+      });
+      const standingsList = document.querySelector(selector);
+      standingsList.innerHTML = card;
+    });
+};
+
+const standingsPage = `<table id="standing-table">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th colspan="2">Club</th>
+                      <th>M</th>
+                      <th class="green-text text-darken-2">W</th>
+                      <th class="red-text text-darken-2">L</th>
+                      <th class="amber-text text-darken-3">Pts</th>
+                    </tr>
+                  </thead>
+                  <tbody class="standings-list"></tbody>
+                </table>`;
+
+const matchPage = `<div class="row match-list"></div>`;
+const standings = () =>
+  requestPage(standingsPage, ".standings-list", "standings", resultsStandings);
+const match = () =>
+  requestPage(matchPage, ".match-list", "matches", resultsMatch);
--- src/js/main.js
+++ src/js/main.js
@@ -4,31 +4,31 @@ document.addEventListener("DOMContentLoaded", function () {
   M.Sidenav.init(document.querySelectorAll(".sidenav"));
 });
 
-document.querySelectorAll(".nav-desktop li i, #nav-mobile i").forEach((elm) => {
-  elm.addEventListener("click", (event) => {
-    page = event.target.parentElement.getAttribute("href").substr(1);
-    loadPage(page);
-  });
-});
+// document.querySelectorAll(".nav-desktop li i, #nav-mobile i").forEach((elm) => {
+//   elm.addEventListener("click", (event) => {
+//     page = event.target.parentElement.getAttribute("href").substr(1);
+//     loadPage(page);
+//   });
+// });
 
-let page = window.location.hash.substr(1);
-if (page === "") page = "standings";
-loadPage(page);
+// let page = window.location.hash.substr(1);
+// if (page === "") page = "standings";
+// loadPage(page);
 
-function loadPage(page) {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      const content = document.querySelector("#body-content");
-      if (this.status === 200) {
-        content.innerHTML = xhttp.responseText;
-      } else if (this.status === 404) {
-        content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-      } else {
-        content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-      }
-    }
-  };
-  xhttp.open("GET", `pages/${page}.html`, true);
-  xhttp.send();
-}
+// function loadPage(page) {
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function () {
+//     if (this.readyState === 4) {
+//       const content = document.querySelector("#body-content");
+//       if (this.status === 200) {
+//         content.innerHTML = xhttp.responseText;
+//       } else if (this.status === 404) {
+//         content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+//       } else {
+//         content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+//       }
+//     }
+//   };
+//   xhttp.open("GET", `pages/${page}.html`, true);
+//   xhttp.send();
+// }
