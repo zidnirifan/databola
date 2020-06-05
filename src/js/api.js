a932cebc375222f0ce38677db303227abc47001e
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-06-05 18:25:04 +0700

    cache diynamic web

--- src/js/api.js
+++ src/js/api.js
@@ -139,6 +139,31 @@ const baseUrl = "https://api.football-data.org/v2";
 const requestPage = (parentSelector, selector, urlKey, results, id) => {
   document.querySelector("#body-content").innerHTML = parentSelector;
 
+  if ("caches" in window) {
+    caches
+      .match(`${baseUrl}/competitions/${id}/${urlKey}`)
+      .then(function (response) {
+        if (response) {
+          response.json().then(function (response) {
+            let items = response;
+            if (urlKey === "matches") {
+              items = response.matches;
+            } else if (urlKey === "standings") {
+              items = response.standings[0].table;
+            } else if (urlKey === "teams") {
+              items = response.teams;
+            }
+            let card = "";
+            items.forEach(function (e) {
+              card += results(e);
+            });
+            const itemList = document.querySelector(selector);
+            itemList.innerHTML = card;
+          });
+        }
+      });
+  }
+
   fetch(`${baseUrl}/competitions/${id}/${urlKey}`, {
     headers: {
       "X-Auth-Token": "dc6ecbe5da084040b9bd5d42e6eb0a42",
--- sw.js
+++ sw.js
@@ -8,12 +8,51 @@ const urlsToCache = [
   "/lib/css/materialize.min.css",
   "/lib/js/materialize.min.js",
   "/assets/icons/Material-Icons.woff2",
+  "/assets/icons/icons.css",
 ];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  const baseUrl = "https://api.football-data.org/v2/";
+
+  if (event.request.url.indexOf(baseUrl) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request, { ignoreSearch: true })
+        .then(function (response) {
+          return response || fetch(event.request);
+        })
+    );
+  }
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log(`ServiceWorker: cache ${cacheName} dihapus`);
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
