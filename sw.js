6637c7cfff1f438eb084325e774397c39a87adf8
Zidni Rif'an <zidni.rifan7@gmail.com>
2020-06-05 16:55:46 +0700

    service-worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "firstpwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/src/css/main.css",
+  "/src/js/main.js",
+  "/src/js/api.js",
+  "/lib/css/materialize.min.css",
+  "/lib/js/materialize.min.js",
+  "/assets/icons/Material-Icons.woff2",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
